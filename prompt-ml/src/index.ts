ee345e950b73b1bda9c5075810f09361ea89dfe9
Galen Harrison <me@galenharrison.com>
2019-11-15 15:33:33 -0600

    Started to modify example - next step, emulate jupyterlab_code_formatter to get cell info

--- prompt-ml/src/index.ts
+++ prompt-ml/src/index.ts
@@ -1,7 +1,14 @@
 import {
   JupyterFrontEnd, JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
+/*
+import {
+  Cell, CodeCell,
+} from "@jupyterlab/cells";
+*/ 
+import {
+  INotebookTracker,
+} from "@jupyterlab/notebook";
 
 /**
  * Initialization data for the prompt-ml extension.
@@ -9,8 +16,19 @@ import {
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'prompt-ml',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension prompt-ml is activated!');
+  activate: (app: JupyterFrontEnd, tracker : INotebookTracker) => {
+    console.log("Hello world");
+/**
+    const cells: CodeCell[] = [];
+    const notebook = tracker.currentWidget.content;
+    notebook.widgets.forEach((cell: Cell) => {
+      if (cell.model.type == "code") {
+        cells.push(cell as CodeCell);
+      }
+    });
+    
+    console.log(cells);  
+*/
   }
 };
 
