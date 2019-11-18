eb03e64e88274e51e0afaa0bf93e3787126e456d
Galen Harrison <me@galenharrison.com>
2019-11-18 16:16:00 -0600

    Partial attempt to get cell listings issue is that notebook panel not loaded yet

---
+++ prompt-ml/src/cell-listener.ts
@@ -0,0 +1,46 @@
+import { 
+  NotebookPanel, 
+  Notebook, 
+} from "@jupyterlab/notebook";
+
+import {
+  ICellModel,
+} from "@jupyterlab/cells";
+
+import { IObservableList } from "@jupyterlab/observables";
+
+import { PromiseDelegate } from "@phosphor/coreutils";
+
+export class CellListen {
+  /*public activeCell: Cell;*/
+  /**
+  this listens on the notebook for changes, and logs them by cell
+  adapted from mkery's Verdant/notebook-listen.ts
+  */
+  constructor(notebookPanel : NotebookPanel) {
+    console.log("init listener");
+    this.notebookPanel = notebookPanel;
+    this.init();
+  }
+
+  private notebook: Notebook;
+  private notebookPanel: NotebookPanel;
+
+  private async init() {
+
+    await this.notebookPanel.revealed;
+    this.notebook = this.notebookPanel.content;
+    this.listen();
+    this._ready.resolve(undefined) /* IDK what this does*/
+  }
+
+  private listen() {
+    this.notebook.model.cells.changed.connect(
+      (sender: any, data: IObservableList.IChangedArgs<ICellModel>) => {
+        console.log("Cell list change type: ", data.type);
+        /*TODO: add change type handling*/
+      });
+  }
+
+  private _ready = new PromiseDelegate<void>();
+}
--- prompt-ml/src/index.ts
+++ prompt-ml/src/index.ts
@@ -1,14 +1,58 @@
 import {
   JupyterFrontEnd, JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-/*
+
 import {
-  Cell, CodeCell,
+  Cell,
 } from "@jupyterlab/cells";
-*/ 
+ 
 import {
   INotebookTracker,
+  INotebookModel,
+  NotebookPanel,
 } from "@jupyterlab/notebook";
+
+import {
+  CellListen,
+} from "./cell-listener";
+
+import {
+    DisposableDelegate, IDisposable,
+} from "@phosphor/disposable";
+
+import {
+  CommandToolbarButton,
+} from "@jupyterlab/apputils";
+
+import {
+    DocumentRegistry,
+} from "@jupyterlab/docregistry";
+
+const ENABLE_HIST_CMD = "prompt_ml_enable";
+
+class CellPrompter implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+
+  private app: JupyterFrontEnd;
+  /*private tracker: INotebookTracker;*/
+ 
+  constructor(app: JupyterFrontEnd, tracker: INotebookTracker) {
+    this.app = app;
+    /*this.tracker = tracker;*/
+    new CellListen(tracker.currentWidget); 
+  }
+
+  public createNew(nb: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+    const btn = new CommandToolbarButton({
+      commands: this.app.commands,
+      id: ENABLE_HIST_CMD,
+    });
+    nb.toolbar.insertAfter("cellType", this.app.commands.label(ENABLE_HIST_CMD), btn);
+      return new DisposableDelegate(() => {
+        btn.dispose();
+     });
+  }
+   
+}
 
 /**
  * Initialization data for the prompt-ml extension.
@@ -16,20 +60,20 @@ import {
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'prompt-ml',
   autoStart: true,
+  requires: [INotebookTracker],
   activate: (app: JupyterFrontEnd, tracker : INotebookTracker) => {
     console.log("Hello world");
-/**
-    const cells: CodeCell[] = [];
-    const notebook = tracker.currentWidget.content;
-    notebook.widgets.forEach((cell: Cell) => {
-      if (cell.model.type == "code") {
-        cells.push(cell as CodeCell);
-      }
-    });
     
-    console.log(cells);  
-*/
+    if (tracker.currentWidget) {
+      tracker.currentWidget.content.widgets.forEach((cell: Cell) => {
+        console.log("cell ", cell.id);
+      }); 
+    }
+
+    const prompter = new CellPrompter(app, tracker);
+    app.docRegistry.addWidgetExtension("Notebook", prompter);
   }
+
 };
 
 export default extension;
