5f97755a65ec0177ff12055c8973ef375dd0a8be
Galen Harrison <me@galenharrison.com>
2020-02-13 13:15:45 -0500

    created script test_build to automatically build server extension and run lab

---
+++ test_build.sh
@@ -0,0 +1,9 @@
+# script to build, install and test jupyter extension
+
+cd ./serverextension
+python3 setup.py sdist bdist_wheel
+pip3 install -U -I dist/prompter-0.1-py3-none-any.whl
+jupyter serverextension enable --py prompter --sys-prefix --debug
+cd ..
+
+jupyter lab .
