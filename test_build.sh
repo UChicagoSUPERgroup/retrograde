2794c1a7224be6e38dc9d6da0f5c4dd8f5a60848
Galen Harrison <me@galenharrison.com>
2020-06-16 14:42:31 -0400

    nuke the cells.db file if it exists on restart

--- test_build.sh
+++ test_build.sh
@@ -6,4 +6,7 @@ pip3 install -U -I dist/prompter-0.1-py3-none-any.whl
 jupyter serverextension enable --py prompter --sys-prefix --debug
 cd ..
 export JUPYTER_CONFIG_DIR="./etc/jupyter"
+
+rm ~/.promptml/cells.db
+
 jupyter lab . --log-level=DEBUG --watch 
