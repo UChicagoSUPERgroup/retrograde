9deb970e61c25ca1eb7d001c304c34623fe0ea41
Galen Harrison <me@galenharrison.com>
2020-06-16 10:02:26 -0400

    added specification to avoid looking in non-workspace directories for lab configuration

--- test_build.sh
+++ test_build.sh
@@ -5,5 +5,5 @@ python3 setup.py sdist bdist_wheel
 pip3 install -U -I dist/prompter-0.1-py3-none-any.whl
 jupyter serverextension enable --py prompter --sys-prefix --debug
 cd ..
-
+export JUPYTER_CONFIG_DIR="./etc/jupyter"
 jupyter lab . --log-level=DEBUG --watch 
