c9fecc2c083559c5ce8f91857030f003afc53c9e
Galen Harrison <me@galenharrison.com>
2020-09-14 14:28:43 -0400

    somehow forgot to commit notifications??

---
+++ jupyter_note_book_plugin/serverextension/prompter/notifications.py
@@ -0,0 +1,297 @@
+import pandas as pd
+import numpy as np
+import dill
+
+from scipy.stats import zscore
+from random import choice
+
+from .storage import load_dfs
+from .sortilege import is_categorical
+
+class Notifications:
+
+    def __init__(self, db):
+        self.db = db
+
+    def feasible(self, cell_id, env):
+        """is it feasible to send this notification?"""
+        # nb. bake in timing + whether prerequisites exist here
+        return False
+   
+    def times_sent(self):
+        """the number of times this notification has been sent"""
+        return 0 
+
+    def make_response(self, env, kernel_id, cell_id):
+        """form the response to send to the frontend""" 
+        raise NotImplementedError
+ 
+class OnetimeNote(Notifications):
+    """
+    A notification that is sent exactly once
+    """
+
+    def __init__(self, db):
+        super().__init__(db)
+        self.sent = False
+
+    def feasible(self, cell_id, env):
+        return not self.sent
+    
+    def times_sent(self):
+        return int(self.sent)
+
+    def make_response(self, env, kernel_id, cell_id):
+        self.sent = True
+
+class SensitiveColumnNote(OnetimeNote):
+
+    """
+    a class that indicates whether a sensitive column is present in
+    an active dataframe
+    """
+
+    def feasible(self, cell_id, env):
+        if super().feasible(cell_id, env):
+            columns = self.db.get_columns("race") # hardcoded for experiment
+            if columns: 
+                self.df_name = columns[0]["name"]
+                return True
+            return False
+        return False
+
+    def make_response(self, env, kernel_id, cell_id):
+
+        super().make_response(env, kernel_id, cell_id)
+
+        resp = {"type" : "resemble"}
+        resp["col"] = "race"
+        resp["category"] = "race"
+        if hasattr(self, "df_name"):
+            resp["df"] = self.df_name
+        else:
+            resp["df"] = "unnamed"
+        self.db.store_response(kernel_id, cell_id, resp)
+
+class ZipVarianceNote(OnetimeNote):
+
+    def feasible(self, cell_id, env):
+        if super().feasible(cell_id, env):
+
+            race_columns = self.db.get_columns("race") # hardcoded for experiment
+            zip_columns = self.db.get_columns("zip")
+
+            if race_columns and zip_columns: 
+                return True
+            return False
+        return False
+
+    def make_response(self, env, kernel_id, cell_id):
+
+        super().make_response(env, kernel_id, cell_id)
+
+        # TODO: what does the response need to be?
+
+        resp = {"type" : "variance"}
+
+        race_columns = self.db.get_columns("race")
+        zip_columns = self.db.get_columns("zip")
+        
+        df_name = None
+ 
+        for r_col in race_columns:
+            for z_col in zip_columns:
+                if r_col["name"] == z_col["name"] and r_col["version"] == z_col["version"]:
+                    df_name = r_col["name"]
+                    df_version = r_col["version"]
+        if not df_name:
+            env._nbapp.log.warning("[NOTIFICATIONS] ZipVarianceNote.make_response: cannot recover dataframe name")
+            return
+        curr_ns = self.db.recent_ns()
+        dfs = load_dfs(curr_ns)
+
+        if df_name not in dfs:
+            env._nbapp.log.warning("[NOTIFICATIONS] ZipVarianceNote.make_response: cannot recover dataframe object of name %s" % df_name)
+            return
+
+        df = dfs[df_name]
+
+        if "race" not in df.columns or "zip" not in df.columns:
+            env._nbapp.log.warning("[NOTIFICATIONS] ZipVarianceNote.make_response: race or zip not in dataframe columns")
+            return
+        panel_df = pd.get_dummies(df["race"])
+        panel_df["zip"] = df["zip"]
+
+        rate_df = panel_df.groupby(["zip"]).mean() 
+
+        resp["zip1"] = 60637 # use this to highlight redlining
+        resp["zip2"] = 60611 
+
+        resp["demo"] = {60637 : int(rate_df["black"][60637]*100), 60611 : int(rate_df["white"][60611]*100)}
+
+        self.db.store_response(kernel_id, cell_id, resp)
+
+class OutliersNote(OnetimeNote):
+
+    def feasible(self, cell_id, env):
+        if super().feasible(cell_id, env):
+
+            columns = self.db.get_columns("principal") # hardcoded for experiment
+            if columns: 
+                return True
+            return False
+
+    def make_response(self, env, kernel_id, cell_id):
+    
+        super().make_response(env, kernel_id, cell_id)
+
+        resp = {"type" : "outliers"}
+        cols = self.db.get_columns("principal")
+      
+        if not cols:
+            env._nbapp.log.warning("[NOTIFICATIONS] OutlierNote.make_response: cannot find column named principal") 
+            return
+
+        df_name = cols[0]["name"]
+        curr_ns = self.db.recent_ns()
+        dfs = load_dfs(curr_ns)
+
+        if df_name not in dfs:
+            env._nbapp.log.warning("[NOTIFICATIONS] OutlierNote.make_response cannot recover dataframe object named %s" % df_name)
+            return
+
+        df = dfs[df_name]
+        scores = np.absolute(zscore(df["principal"]))
+        index = np.argmax(scores)
+
+        resp["col_name"] = "principal"
+        resp["value"] = float(df["principal"].iloc[index])
+        resp["std_dev"] = float(scores[index])
+
+        self.db.store_response(kernel_id, cell_id, resp) 
+
+class PerformanceNote(Notifications):
+
+    def feasible(self, cell_id, env):
+
+        cell_code = self.db.get_code(env._kernel_id, cell_id)
+        if not cell_code: return False
+
+        poss_models = env.get_models(cell_code)
+        ns = self.db.recent_ns()
+        dfs = load_dfs(ns)
+
+        ns = dill.loads(ns["namespace"])
+
+        models = []
+        env._nbapp.log.debug("[PERFORMANCENOTE] there are {0} possible models".format(len(poss_models)))
+ 
+        for model_name in poss_models:
+
+            features_name = poss_models[model_name]["features"]["name"]
+            labels_name = poss_models[model_name]["label"]["name"]
+
+            has_model = model_name in ns.keys()
+            has_features = features_name in dfs.keys() # implicit expectation that arguments are dataframes
+            has_labels = labels_name in dfs.keys()
+
+            env._nbapp.log.debug(
+                "[PERFORMANCENOTE] model {0} has_model {1}, has_features {2}, has_labels {3}".format(model_name, has_model, has_features, has_labels))
+
+            if (has_model and has_features and has_labels):
+                models.append((model_name, ns[model_name], dfs[features_name], 
+                               dfs[labels_name], poss_models[model_name]["features"]["name_ind"],
+                               poss_models[model_name]["label"]["name_ind"]))
+
+        self.models = models
+
+        if models: return True
+        return False 
+
+    def times_sent(self):
+        if not hasattr(self, "sent"):
+            self.sent = 0
+        return self.sent
+
+    def new_model_name(self, kernel_id, cell_id, model_name):
+        """are we updating an old model perf or creating a new one?"""
+        responses = self.db.get_responses(kernel_id)
+
+        if cell_id not in responses: return None
+
+        for resp in responses[cell_id]: 
+            if resp["type"] == "model_perf" and resp["model_name"] == model_name:
+                return resp
+        return None
+
+    def make_response(self, env, kernel_id, cell_id):
+
+        model_name, model, features, labels, features_list, labels_list = choice(self.models) # lets mix it up a little
+
+        resp = {"type" : "model_perf"}
+        resp["model_name"] = model_name
+
+        if features_list:
+            if len(features_list) == 1:
+                features_df = features[features_list[0]]
+            else:
+                features_df = features[features_list]
+        else:
+            features_df = features
+
+        env._nbapp.log.debug("[PERFORMANCENOTE] features_list {0}".format(features_list))
+        env._nbapp.log.debug("[PERFORMENCENOTE] Input columns {0}".format(features_df.columns))
+
+        if labels_list:
+            if len(labels_list) == 1:
+                labels_df = labels[labels_list[0]]
+            else:
+                labels_df = labels[labels_list]
+        else:
+            labels_df = labels
+       
+        subgroups = []
+         
+        if "race" in features.columns: 
+            subgroups.append("race")
+        if "sex" in features.columns:
+            subgroups.append("sex")
+
+        if len(subgroups) == 0: # fall back to this option
+            env._nbapp.log.debug("[NOTIFICATIONS] PerfNote.make_response, cannot find sensitive columns falling back to categorical vars")
+            subgroups = [c for c in column if is_categorical(features[c])]
+
+        env._nbapp.log.debug("[NOTIFICATIONS] Perfnote.make_response analyzing columns {0}".format(subgroups))
+
+        preds = model.predict(features_df)
+      
+        pos = model.classes_[0]
+        neg = model.classes_[1]
+
+        resp["values"] = {"pos" : str(pos), "neg" : str(neg)}
+        resp["columns"] = {}
+ 
+        for col_name in subgroups:
+
+            resp["columns"][col_name] = {} 
+
+            for val in features[col_name].unique():
+
+                mask = (features[col_name] == val)
+
+                fp = sum((preds[mask] == pos) & (labels_df[mask] == neg))
+                fp = fp/(sum(labels_df[mask] == neg))
+
+                fn = sum((preds[mask] == neg) & (labels_df[mask] == pos))
+                fn = fn/(sum(labels_df[mask] == pos))
+
+                resp["columns"][col_name][str(val)] = {"fpr" : fp, "fnr" : fn}
+
+        env._nbapp.log.debug("[PERFORMANCENOTE] notification is {0}".format(resp))
+
+        old_resp = self.new_model_name(kernel_id, cell_id, model_name) 
+
+        if not old_resp: 
+            self.db.store_response(kernel_id, cell_id, resp) 
+        else:
+            self.db.update_response(kernel_id, cell_id, old_resp, resp) 
