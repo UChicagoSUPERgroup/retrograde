772242c8f1c67e269f7cf3bf0855a070f183466d
Galen Harrison <me@galenharrison.com>
2020-11-20 11:49:39 -0500

    bump image version

--- notebook-server/app/make_notebook/__init__.py
+++ notebook-server/app/make_notebook/__init__.py
@@ -4,7 +4,7 @@ from random import randint
 import time
 import secrets
 
-IMAGE = 'gsamharrison/plugin-test:1.9'
+IMAGE = 'gsamharrison/plugin-test:1.9.1'
 
 def get_free_port():
     lower_port = 8000
