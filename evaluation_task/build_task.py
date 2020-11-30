0d349891f1fa5a45cd4d1b08fa32c3fd542eb0a7
Galen Harrison <me@galenharrison.com>
2020-11-30 14:20:43 -0500

    changes based on piloting to data task

--- evaluation_task/build_task.py
+++ evaluation_task/build_task.py
@@ -7,10 +7,13 @@ with open(f'{TASK_NAME}.ipynb', 'r') as f:
 	data = json.load(f)
 
 for cell in data['cells']:
-	if cell['cell_type'] == 'markdown':
-		cell['metadata'] = {'trusted':True,
-		                    'editable': False,
-		                    'deletable': False}
+    if cell['cell_type'] == 'markdown':
+        cell['metadata'] = {'trusted':True,
+                            'editable': False,
+                            'deletable': False}
+    if cell["cell_type"] == "code":
+        cell["outputs"] = []
+        cell["execution_count"] = None
 
 with open(f'{TASK_NAME}_build.ipynb', 'w') as f:
 	json.dump(data, f)
