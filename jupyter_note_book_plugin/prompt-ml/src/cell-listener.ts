6fbdc2f56d30d76d2b19574373521c1eb5b2fd4e
biniona <biniona@uchicago.edu>
2021-01-06 11:47:54 -0500

    delete unused example CellListen class

--- jupyter_note_book_plugin/prompt-ml/src/cell-listener.ts
+++ jupyter_note_book_plugin/prompt-ml/src/cell-listener.ts
@@ -112,51 +112,5 @@ export class Listener {
         }
       })
   }
-  
-  
-  private _ready = new PromiseDelegate<void>();
-}
-export class CellListen {
-  /*public activeCell: Cell;*/
-  /**
-  this listens on the notebook for changes, and logs them by cell
-  adapted from mkery's Verdant/notebook-listen.ts
-  */
-  constructor(notebookPanel : NotebookPanel) {
-    console.log("init listener");
-    this.notebookPanel = notebookPanel;
-    console.log("nbpanel = ",notebookPanel);
-    this.init();
-  }
-
-  private notebook: Notebook;
-  private notebookPanel: NotebookPanel;
-
-  private async init() {
-
-    await this.notebookPanel.revealed;
-    console.log("notebookpanel revealed");
-    this.notebook = this.notebookPanel.content;
-    console.log("notebook = ", this.notebook);
-    this.listen();
-    this._ready.resolve(undefined) /* IDK what this does*/
-  }
-
-  private listen() {
-
-    console.log("listening");
-    console.log("changed = ",this.notebook.model.cells.changed);
-
-    this.notebook.model.cells.changed.connect(
-      (sender: any, data: IObservableList.IChangedArgs<ICellModel>) => {
-        console.log("Cell list change type: ", data.type);
-        /*TODO: add change type handling*/
-      });
-    this.notebook.model.contentChanged.connect(
-      (sender: any, data: any) => {
-        console.log("notebook changed", data);
-    });
-  }
-
   private _ready = new PromiseDelegate<void>();
 }
