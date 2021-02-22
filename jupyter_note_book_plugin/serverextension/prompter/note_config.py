722d2dba32f319cbce8c36e576d98cdb56db973d
Galen Harrison <me@galenharrison.com>
2021-02-22 10:26:09 -0500

    Filled in sets of rules for EXP_CTS mode for build

--- jupyter_note_book_plugin/serverextension/prompter/note_config.py
+++ jupyter_note_book_plugin/serverextension/prompter/note_config.py
@@ -1,12 +1,12 @@
-from .notifications import ProtectedColumnNote, ZipVarianceNote, OutliersNote, PerformanceNote, EqualizedOddsNote, ProxyColumnNote
+from .notifications import ProtectedColumnNote, OutliersNote, EqualizedOddsNote, ProxyColumnNote
 from .config import MODE
 
 if MODE == "EXP_CTS":
     NOTE_RULES = {
         "intro" :[],
         "tutorial" : [OutliersNote],
-        "null_clean" : [],
-        "model" : [ZipVarianceNote, EqualizedOddsNote],
+        "null_clean" : [ProtectedColumnNote, ProxyColumnNote],
+        "model" : [EqualizedOddsNote],
         "end" : []
     }
 if MODE == "EXP_END":
