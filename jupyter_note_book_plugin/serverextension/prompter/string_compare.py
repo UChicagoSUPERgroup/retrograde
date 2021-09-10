4a56c1737c639e1f86860dee6b67eb66b21f7439
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2021-09-10 19:37:17 -0400

    cleaning up function logic

--- jupyter_note_book_plugin/serverextension/prompter/string_compare.py
+++ jupyter_note_book_plugin/serverextension/prompter/string_compare.py
@@ -6,7 +6,7 @@ PROTECTED_PROXY_MATCH_THRESHOLD = 80
 PATH_PROTECTED_JSON = './protected_columns.json'
 PATH_PROTECTED_JSON_FULL = 'evaluation_task/build/protected_columns.json'
 
-PATTERN_THRESHOLD = 0.8
+COLUMN_PATTERN_THRESHOLD = 0.8
 
 
 def check_for_protected(column_names):
@@ -44,7 +44,54 @@ def guess_protected(dataframe):
 
         Pregnancy: none
         """
-        pass
+
+        # this process may flag the same column multiple times as potentially
+        # protected, so we should use a set to 
+        results = set()
+
+        # gender/sex
+        for column in dataframe:
+            words = ['male', 'female', 'm', 'f', 'man', 'woman', 'boy', 'girl', 
+                     'non-binary', 'nb']
+            level_match = _string_column_vs_list(dataframe, column, words, 
+                                                 PROTECTED_MATCH_THRESHOLD)
+            if level_match >= COLUMN_PATTERN_THRESHOLD:
+                results.add(level_match)
+        
+        # race/color
+        for column in dataframe:
+            words = ['black', 'white', 'red', 'yellow', 'brown', 'african', 
+                     'asian', 'caucasian', 'indigenous', 'native', 'hispanic', 
+                     'pacific islander']
+            level_match = _string_column_vs_list(dataframe, column, words, 
+                                                 PROTECTED_MATCH_THRESHOLD)
+            if level_match >= COLUMN_PATTERN_THRESHOLD:
+                results.add(level_match)
+
+        # religion
+        for column in dataframe:
+            words = ['christianity', 'christian', 'islam', 'muslim', 'judaism', 
+                     'jewish', 'buddhism', 'buddhist', 'hinduism', 'hindu', 
+                     'atheist', 'agnostic', 'non-believer', 'folk', 'none']
+            level_match = _string_column_vs_list(dataframe, column, words, 
+                                                 PROTECTED_MATCH_THRESHOLD)
+            if level_match >= COLUMN_PATTERN_THRESHOLD:
+                results.add(level_match)
+
+        # age
+        # TODO
+
+        # sexual orientation
+        for column in dataframe:
+            words = ['gay', 'straight', 'pansexual', 'pan', 'bisexual', 'bi', 
+                     'heterosexual', 'homosexual', 'ace', 'asexual', 
+                     'allosexual', 'alloromantic', 'aromantic', 'aro', 
+                     'questioning', 'lesbian']
+            level_match = _string_column_vs_list(dataframe, column, words, 
+                                                 PROTECTED_MATCH_THRESHOLD)
+            if level_match >= COLUMN_PATTERN_THRESHOLD:
+                results.add(level_match)
+        return list(results)
 
 def _get_protected():
     '''read in the protected values corpus'''
@@ -69,3 +116,23 @@ def _fuzzy_string_across_dict(candidate_string, reference_dict, threshold):
                             "protected_value_background" : v,
                             "original_name" : candidate_string})
     return results
+
+# see if a string fuzzy matches any of the words in the words
+# list and return a dict with the result and matched word
+def _match_any_string(string, words, threshold):
+    for word in words:
+        partial_match = fuzz.partial_ratio(string.lower(), word)
+        if partial_match >= threshold:
+            # might be useful to save the actual word it matched to
+            return {'match': True, 'value': word}
+    return {'match': False, 'value': ''}
+
+# count the number of values in this column match any string in
+# the 'words' list
+def _string_column_vs_list(dataframe, colname, words, threshold):
+    count = 0
+    for row in dataframe.iterrows():
+        didmatch = _match_any_string(str(row[colname]), words, threshold)
+        if didmatch['match']:
+            count += 1
+    return float(count) / dataframe.shape[0]
